556db10f785dbebe96b3cc184f932593af21b6cc
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-09-27 13:11:22 -0400

    feat: movie card with redirection added as atom module

--- web-frontend/src/components/organisms/movies/MoviesRenderer.jsx
+++ web-frontend/src/components/organisms/movies/MoviesRenderer.jsx
@@ -1,11 +1,14 @@
-import MovieCard from "@/components/molecules/movies/MovieCard";
-import "@/styles/layout/_movies_layout.scss"
+import MovieCardWithRedirection from "@/components/molecules/movies/MovieCardWithRedirection";
+import "@/styles/layout/_movies_layout.scss";
 
 const MoviesRenderer = ({ movies }) => {
     return (
         <article className="movies-wrapper">
             {movies.map((movie, i) => (
-                <MovieCard movie={movie} key={`movie-card-${i}`} />
+                <MovieCardWithRedirection
+                    movie={movie}
+                    key={`movie-card-${i}`}
+                />
             ))}
         </article>
     );
