e242885fca69851e58f560e43ddf8a498c71fc00
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-09-28 01:40:05 -0400

    feat: add MoveRepository class with its controller

---
+++ WebApi/src/Infraestructure/Repositories/Concrete/MovieRepository.cs
@@ -0,0 +1,31 @@
+using Domain;
+using Infraestructure.Data;
+using Infraestructure.Repositories.Abstractions;
+using MongoDB.Driver;
+
+namespace Infraestructure.Repositories;
+
+public class MovieRepository : IMovieRepository
+{
+    private readonly IMongoDatabase _database;
+    private readonly IMongoCollection<Movie> _movieCollection;
+
+    public MovieRepository(DatabaseConfig databaseConfig)
+    {
+        _database = databaseConfig.GetDatabase();
+        _movieCollection = _database.GetCollection<Movie>("movies");
+    }
+
+    public List<Movie> GetActiveMovies()
+    {
+        var filter = Builders<Movie>.Filter.Eq(m => m.IsActive, true);
+        var activeMovies = _movieCollection.Find(filter).ToList();
+        return activeMovies;
+    }
+
+    public List<Movie> GetAllMovies()
+    {
+        return _movieCollection.Find(d => true).ToList();
+    }
+
+}
