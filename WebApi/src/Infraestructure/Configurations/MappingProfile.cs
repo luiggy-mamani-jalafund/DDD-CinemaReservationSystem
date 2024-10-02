4cf49dc2a4c94d5f02fd6042a7434e25dbad6843
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-10-02 12:41:56 -0400

    feat: added theater and seat mapping

--- WebApi/src/Infraestructure/Configurations/MappingProfile.cs
+++ WebApi/src/Infraestructure/Configurations/MappingProfile.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
 using Domain;
+using WebApi.src.Domain.Entities.Theaters;
 using WebApi.src.Infraestructure.Dtos;
+using WebApi.src.Infraestructure.Dtos.TheatersDtos;
 
 namespace WebApi.src.Infraestructure.Configurations;
 
@@ -8,6 +10,19 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
+        MappingMovies();
+        MappingTheaters();
+    }
+
+    private void MappingMovies()
+    {
         CreateMap<Movie, MovieDto>();
+    }
+
+    private void MappingTheaters()
+    {
+        CreateMap<Theater, TheaterDto>();
+        CreateMap<SeatingSet, SeatingSetDto>();
+        CreateMap<Seat, SeatDto>();
     }
 }
