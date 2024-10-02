b45b17fbf8d9816ac4c16ae27d9b754b0edcd09a
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-10-02 12:42:37 -0400

    feat: added dependencies for theater repository and service

--- WebApi/src/Infraestructure/Configurations/DependencyInjector.cs
+++ WebApi/src/Infraestructure/Configurations/DependencyInjector.cs
@@ -2,6 +2,8 @@ using Infraestructure.Repositories;
 using Infraestructure.Repositories.Abstractions;
 using WebApi.src.Application.UseCaseAbstractions;
 using WebApi.src.Application.UseCases;
+using WebApi.src.Domain.Repositories;
+using WebApi.src.Infraestructure.Repositories;
 
 namespace WebApi.src.Infraestructure.Configurations;
 
@@ -16,11 +18,13 @@ public static class DependencyInjector
     private static void InjectRepositories(IServiceCollection services)
     {
         services.AddScoped<IMovieRepository, MovieRepository>();
+        services.AddScoped<ITheaterRepository, TheaterRepository>();
     }
 
     private static void InjectServices(IServiceCollection services)
     {
         services.AddScoped<IMovieBillboardService, MovieBillboardService>();
+        services.AddScoped<ITheaterService, TheaterService>();
     }
 
 }
