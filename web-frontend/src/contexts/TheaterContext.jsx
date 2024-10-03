09375917f4b2552c586eb397372c26c6ee16b767
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-10-02 20:57:31 -0400

    feat: implemented theater context for theater state management

---
+++ web-frontend/src/contexts/TheaterContext.jsx
@@ -0,0 +1,87 @@
+"use client";
+import { createContext, useState } from "react";
+
+export const TheaterContext = createContext();
+
+const DeafultState = {
+    theater: undefined,
+    selectedSeats: [],
+    seatsOccupied: [],
+};
+
+const SeatState = {
+    ocupied: "ocupied",
+    selected: "selected",
+    free: "free",
+};
+
+const TheaterProvider = ({ children }) => {
+    const [theaterState, setTheaterState] = useState(DeafultState);
+
+    const setTheater = (theater) => {
+        setTheaterState((prev) => ({ ...prev, theater }));
+    };
+
+    const selectSeat = (seat) => {
+        setTheaterState((prev) => ({
+            ...prev,
+            seatsOccupied: [...prev.seatsOccupied, seat],
+        }));
+    };
+
+    const deselectSeat = (seat) => {
+        setTheaterState((prev) => ({
+            ...prev,
+            seatsOccupied: prev.seatsOccupied.filter(
+                (selectedSeat) => selectedSeat.seatId !== seat.seatId,
+            ),
+        }));
+    };
+
+    const isSeatOccupied = (seat) => {
+        return containsInSeatingSet(seat, theaterState.seatsOccupied);
+    };
+
+    const isSeatSelected = (seat) => {
+        return containsInSeatingSet(seat, theaterState.selectedSeats);
+    };
+
+    const getSeatState = (seat) => {
+        if (isSeatOccupied(seat)) {
+            return SeatState.ocupied;
+        }
+
+        if (isSeatSelected(seat)) {
+            return SeatState.selected;
+        }
+
+        return SeatState.free;
+    };
+
+    const values = {
+        theaterState,
+        theater: theaterState.theater,
+        selectedSeats: theaterState.selectedSeats,
+        seatsOccupied: theaterState.seatsOccupied,
+        setTheater,
+        setTheaterState,
+        selectSeat,
+        deselectSeat,
+        getSeatState,
+    };
+
+    return (
+        <TheaterContext.Provider value={values}>
+            {children}
+        </TheaterContext.Provider>
+    );
+};
+
+export default TheaterProvider;
+
+function containsInSeatingSet(seat, seatingSet) {
+    return seatingSet.reduce(
+        (acc, seatOccupied) => acc || seatOccupied.seatId === seat.seatId,
+        false,
+    );
+}
