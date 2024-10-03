06f0eb851a97fc6a25c02579ebef78bdb45e32a1
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-10-03 17:32:48 -0400

    feat: implemented functionallity to fetch theater schedule form query

--- web-frontend/src/components/template/theaters/Theater.jsx
+++ web-frontend/src/components/template/theaters/Theater.jsx
@@ -1,15 +1,40 @@
+"use client";
+
 import TypesSeatState from "@/components/molecules/theaters/TypesSeatState";
 import TheaterRenderer from "@/components/organisms/theaters/TheaterRenderer";
+import { TheaterContext } from "@/contexts/TheaterContext";
 import "@/styles/layout/_theater_layout.scss";
 import "@/styles/other/_theater_items.scss";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useContext } from "react";
 
-const Theater = ({ theaterId }) => {
+const Theater = () => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const { schedule, setSchedule } = useContext(TheaterContext);
+
+    useEffect(() => {
+        const scheduleParam = searchParams.get("schedule");
+
+        if (scheduleParam) {
+            try {
+                const decodedSchedule = decodeURIComponent(scheduleParam);
+                const parsedSchedule = JSON.parse(decodedSchedule);
+                setSchedule(parsedSchedule);
+            } catch (_) {
+                router.back();
+            }
+        }
+    }, [searchParams]);
+
+    if (!schedule) return <p>Loading...</p>;
+
     return (
         <main className="main-theater-wrapper">
             <div className="theater-screen"></div>
             <div className="theater-screen-text">screen</div>
-            <TheaterRenderer theaterId={theaterId} />
-            <TypesSeatState/>
+            <TheaterRenderer theaterId={schedule.theaterId} />
+            <TypesSeatState />
         </main>
     );
 };
