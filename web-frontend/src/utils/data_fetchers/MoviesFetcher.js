d5152e64d8a77436cf1d292b961750aa052bc416
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-10-03 08:46:21 -0400

    refactor: fix url movies

--- web-frontend/src/utils/data_fetchers/MoviesFetcher.js
+++ web-frontend/src/utils/data_fetchers/MoviesFetcher.js
@@ -1,19 +1,19 @@
 export async function fetchSingleMovie(movieId) {
-    const response = await fetch(`http://localhost:5011/api/Movie/${movieId.id}`);
+    const response = await fetch(`http://localhost:5011/api/movies/${movieId.id}`);
     const movie = await response.json();
     
     return movie;
 }
 
 export async function fetchAvailableMovies() {
-    const response = await fetch("http://localhost:5011/api/Movie/active");
+    const response = await fetch("http://localhost:5011/api/movies/active");
     const movies = await response.json();
     
     return movies
 }
 
 export async function fetchMovieShowtimes(movieId) {
-    const response = await fetch(`http://localhost:5011/api/Showtime/${movieId.id}`);
+    const response = await fetch(`http://localhost:5011/api/showtime/${movieId.id}`);
     const showtimes = await response.json();
     
     return showtimes;
