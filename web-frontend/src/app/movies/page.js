392274d02d95fe9fb24dc25a4a9f6fe56866f693
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-10-07 10:51:43 -0400

    fix: move fetchAvalableMovies to MovieBillboard

--- web-frontend/src/app/movies/page.js
+++ web-frontend/src/app/movies/page.js
@@ -1,8 +1,6 @@
 import MovieBillboard from "@/components/template/movies/MovieBillboard";
-import { fetchAvailableMovies } from "@/utils/data_fetchers/MoviesFetcher";
 
 export default async function MoviesPage() {
-    const availableMovies = await fetchAvailableMovies();
 
-    return <MovieBillboard movies={availableMovies} />;
+    return <MovieBillboard />;
 }
