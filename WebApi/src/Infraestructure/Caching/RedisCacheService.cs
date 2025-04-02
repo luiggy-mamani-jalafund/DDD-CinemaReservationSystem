5c23322922387f3029556305dfb6205abcc621e4
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2025-04-02 18:26:30 -0400

    feat: added caching handling

---
+++ WebApi/src/Infraestructure/Caching/RedisCacheService.cs
@@ -0,0 +1,36 @@
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace Infraestructure.Cache
+{
+    public class RedisCacheService : ICacheService
+    {
+        private readonly IDatabase _redisDatabase;
+
+        public RedisCacheService(IConnectionMultiplexer redis)
+        {
+            _redisDatabase = redis.GetDatabase();
+        }
+
+        public async Task<T?> GetAsync<T>(string key)
+        {
+            var value = await _redisDatabase.StringGetAsync(key);
+            if (value.HasValue)
+            {
+                return JsonSerializer.Deserialize<T>(value);
+            }
+            return default;
+        }
+
+        public async Task SetAsync<T>(string key, T value, TimeSpan? expiry = null)
+        {
+            var json = JsonSerializer.Serialize(value);
+            await _redisDatabase.StringSetAsync(key, json, expiry);
+        }
+
+        public async Task RemoveAsync(string key)
+        {
+            await _redisDatabase.KeyDeleteAsync(key);
+        }
+    }
+}
