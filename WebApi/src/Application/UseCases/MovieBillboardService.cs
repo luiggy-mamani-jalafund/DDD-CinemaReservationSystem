a489630f12d663879f1ae0a7218e9dbd2d6bfbe1
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2025-04-02 18:27:31 -0400

    feat: added caching when movies is required

--- WebApi/src/Application/UseCases/MovieBillboardService.cs
+++ WebApi/src/Application/UseCases/MovieBillboardService.cs
@@ -1,21 +1,35 @@
 using Domain;
 using Infraestructure.Repositories.Abstractions;
 using Application.UseCaseAbstractions;
+using Infraestructure.Cache;
 
 namespace Application.UseCases;
 
 public class MovieBillboardService : IMovieBillboardService
 {
     private readonly IMovieRepository _movieRepository;
+    private readonly ICacheService _cacheService;
 
-    public MovieBillboardService(IMovieRepository movieRepository)
+    public MovieBillboardService(IMovieRepository movieRepository,
+                                    ICacheService cacheService)
     {
         _movieRepository = movieRepository;
+        _cacheService = cacheService;
     }
 
-    public List<Movie> GetActiveMovies()
+    public async Task<List<Movie>> GetActiveMovies()
     {
+        const string cacheKey = "active_movies";
+        var cachedMovies = await _cacheService.GetAsync<List<Movie>>(cacheKey);
+
+        if (cachedMovies != null)
+        {
+            return cachedMovies;
+        }
+
         var movies = _movieRepository.GetActiveMovies();
+
+        await _cacheService.SetAsync(cacheKey, movies, TimeSpan.FromMinutes(5));
 
         return movies;
     }
